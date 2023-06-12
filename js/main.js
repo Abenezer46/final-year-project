799667add9874bf3e9d082f35915819e92ceaf75
Abenezer46 <75671726+Abenezer46@users.noreply.github.com>
2023-06-12 15:48:04 +0300

    javascript file fix removed promet

--- js/main.js
+++ js/main.js
@@ -4,13 +4,6 @@
             mouseMoved = true;
         });
 
-        // Set a timer to check for inactivity
-        var inactivityTimer = setTimeout(function () {
-            // Show a prompt after 30 seconds of inactivity
-            if (!mouseMoved) {
-                alert('Are you there?');
-            }
-        }, 30000);
 
         // Set a timer to redirect to the login page after 60 seconds of inactivity
         var redirectTimer = setTimeout(function () {
@@ -18,21 +11,15 @@
             if (!mouseMoved) {
                 window.location.href = './pages/login.php';
             }
-        }, 60000);
+        }, 30000);
 
         // Reset the timers on mouse movement
         document.addEventListener('mousemove', function () {
             mouseMoved = true;
-            clearTimeout(inactivityTimer);
             clearTimeout(redirectTimer);
-            inactivityTimer = setTimeout(function () {
-                if (!mouseMoved) {
-                    alert('Are you there?');
-                }
-            }, 30000);
             redirectTimer = setTimeout(function () {
                 if (!mouseMoved) {
                     window.location.href = './pages/login.php';
                 }
-            }, 60000);
+            }, 30000);
         });
